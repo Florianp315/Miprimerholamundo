4b5a05c4440dc21b91da7048d296bab43b80d190
cflorian <camiloflorian14@gmail.com>
2024-02-09 22:15:52 -0500

    Actualizado1

---
+++ HolaMiPrimerMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaMiPrimerMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mi nombre es Camilo Florian, Espero esten muy bien");
+        }
+    }
+}
